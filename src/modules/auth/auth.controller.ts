e07212755aa8c24490bae0fb39d3964a5d759f02
vule <vulehuy2905@gmail.com>
2025-02-12 23:49:00 +0700

    refactor: refactoring the success responses

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,19 +1,25 @@
 import { Request, Response } from 'express';
 import { authService } from '~/services/db/auth.service';
 import logger from '~/utils/logger';
-import { successResponse } from '~/utils/success';
+import { OkResponse } from '~/utils/success';
 
 class AuthController {
   public login = async (req: Request, res: Response) => {
     logger.info(`AuthController.login - request received`);
     const { username, password } = req.body;
     const { accessToken, refreshToken } = await authService.login(username, password);
-    successResponse({ accessToken, refreshToken }, res);
+    new OkResponse({
+      message: 'Login successfully',
+      metadata: { accessToken, refreshToken }
+    }).send(res);
   };
   public register = async (req: Request, res: Response) => {
     logger.info(`AuthController.register - request received`);
     const user = await authService.register(req.body);
-    successResponse(user, res);
+    new OkResponse({
+      message: 'Registered successfully',
+      metadata: user
+    }).send(res);
   };
 }
 
--- src/utils/success.ts
+++ src/utils/success.ts
@@ -1,8 +1,42 @@
-import { Response } from 'express';
+import type { Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
-const successResponse = (data: unknown, res: Response) => {
-  res.status(StatusCodes.OK).json({ data });
-};
+interface ISuccessResponse {
+  message: string | number;
+  statusCode: number;
+  reasonStatusCode: number;
+  metadata: object | string | number;
+}
 
-export { successResponse };
+class SuccessResponse {
+  message: string | number;
+  statusCode: number;
+  metadata: object | string | number;
+
+  constructor({ message, statusCode, reasonStatusCode, metadata = {} }: ISuccessResponse) {
+    this.message = !message ? reasonStatusCode : message;
+    this.statusCode = statusCode;
+    this.metadata = metadata;
+  }
+
+  send(res: Response) {
+    res.status(this.statusCode).json(this);
+  }
+}
+
+class OkResponse extends SuccessResponse {
+  constructor({ message, metadata }: Pick<SuccessResponse, 'message' | 'metadata'>) {
+    super({ message, statusCode: StatusCodes.OK, reasonStatusCode: StatusCodes.OK, metadata });
+  }
+}
+
+class CreatedResponse extends SuccessResponse {
+  options: object;
+
+  constructor({ message, metadata, options }: Pick<SuccessResponse, 'message' | 'metadata'> & { options: object }) {
+    super({ message, statusCode: StatusCodes.CREATED, reasonStatusCode: StatusCodes.CREATED, metadata });
+    this.options = options;
+  }
+}
+
+export { CreatedResponse, OkResponse };
